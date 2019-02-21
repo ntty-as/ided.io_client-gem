d46de1a7f1ef07460598fc4a8cffe3f7f7ccbe2c
Max Brosnahan <maximilianbrosnahan@gmail.com>
2019-02-21 15:19:08 +0100

    Add helper to set the site param correctly

--- lib/ided_client/base_resource.rb
+++ lib/ided_client/base_resource.rb
@@ -5,5 +5,9 @@ module IdedClient
     def self.with_credential(cred, &block)
       with_headers(authorization: "Bearer #{cred.access_token}", &block)
     end
+
+    def self.host=(host)
+      self.site = "#{host}/api/v1"
+    end
   end
 end
