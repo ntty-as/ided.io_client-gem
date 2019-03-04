77bd6db815dfae624d9b96ecd49253a747ed2806
Max Brosnahan <maximilianbrosnahan@gmail.com>
2019-03-04 09:07:25 +0100

    Default to production ided.io
    Why: So that consumers of this gem do not need to think about which environment they are connecting to.

--- lib/ided_client/wrapper.rb
+++ lib/ided_client/wrapper.rb
@@ -18,7 +18,7 @@ module IdedClient
     end
 
     def ided_host
-      @ided_host ||= ENV.fetch("IDED_HOST")
+      @ided_host ||= ENV.fetch("IDED_HOST", "https://auth.ided.io")
     end
 
     private
